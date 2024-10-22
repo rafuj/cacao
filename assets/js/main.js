543237b3adf6e618d31000823f6bdb5f6edb8b28
rafuj <raihanrafuj9978@gmail.com>
2024-10-23 00:16:42 +0600

    routine section web done

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,5 +33,30 @@
 		$(".cacao-nos-marques__arrows-next").click(function () {
 			maquesSwiper.slideNext();
 		});
+
+		var routineSwiper = new Swiper(".cacao-routine__slider", {
+			loop: true,
+			spaceBetween: 0,
+			breakpoints: {
+				0: {
+					slidesPerView: 2,
+				},
+				576: {
+					slidesPerView: 2,
+				},
+				768: {
+					slidesPerView: 3,
+				},
+				1024: {
+					slidesPerView: 4,
+				},
+			},
+		});
+		$(".cacao-routine__arrows-prev").click(function () {
+			routineSwiper.slidePrev();
+		});
+		$(".cacao-routine__arrows-next").click(function () {
+			routineSwiper.slideNext();
+		});
 	});
 })(jQuery);
